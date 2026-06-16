7406236d29d682358ce6962faa126ebced834310
Camryn Osofsky <closofsky@gmail.com>
2026-06-16 16:55:38 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # make a hashmap using the sorted strings as keys and their indices as values
+        # initialize hashmap
+        myMap = {}
+
+        # loop through strings, 
+        for s in strs:
+            sortedStr = ''.join(sorted(s))
+            if sortedStr in myMap:
+                myMap[sortedStr].append(s)
+            else:
+                myMap[sortedStr] = [s]
+
+        return list(myMap.values())
