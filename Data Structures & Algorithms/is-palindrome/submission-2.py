236ff566e400d22ed18964e0ff569262807d5b45
Camryn Osofsky <closofsky@gmail.com>
2026-09-16 19:13:50 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,32 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # handle edge cases (lengths 1 and 0)
+        if len(s) == 0:
+            return False
+        elif len(s) == 1:
+            return True
+        
+        # first we need to strip all punctuation, whitespace, and turn to lowercase
+        cleanStr = re.sub(r'[\W_]+', '', s)
+        cleanStr = cleanStr.lower()
+        
+        # now use 2 pointers to work from the outside in
+        # make sure to check if the pointers have passed each other, this is where the loop will end
+        # if the values at the pointers are not equal, break
+        # else, update the pointer values and continue
+
+        i = 0
+        j = len(cleanStr) - 1
+
+        while (i < j):
+            if cleanStr[i] != cleanStr[j]:
+                return False
+            
+            i += 1
+            j -= 1
+
+        return True
+
+
